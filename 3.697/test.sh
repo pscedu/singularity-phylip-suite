777519bb21bc53462c066bc35eafa87e7aba7e90
Ivan Cao-Berg <icaoberg@psc.edu>
2021-06-21 22:41:11 -0400

    Updated test script

---
+++ 3.697/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+module load phylip-suite
+
+cat << EOF > input.file
+data/protdist.input
+Y
+EOF
+
+protdist < input.file
+
+if [ -f input.file ]; then
+	rm -f input.file
+fi
+
+if [ -f output ]; then
+	mv output protdist.output
+fi
